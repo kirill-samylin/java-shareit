4eb335d331beb556821433522228233ed7c698a8
ksamylin <kirill-itb@yandex.ri>
2025-08-22 13:27:00 +0300

    fix: check style

--- src/main/java/ru/practicum/shareit/booking/dto/CreateBookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/CreateBookingDto.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.booking.dto;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-import ru.practicum.shareit.booking.BookingStatus;
 
 import java.time.LocalDateTime;
 
