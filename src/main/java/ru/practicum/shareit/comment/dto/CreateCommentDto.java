8da06fe66ddb921fa0dc2bbe41b13455d65aa6ae
ksamylin <kirill-itb@yandex.ri>
2025-08-29 10:06:16 +0300

    fix: add validation to create comment

--- src/main/java/ru/practicum/shareit/comment/dto/CreateCommentDto.java
+++ src/main/java/ru/practicum/shareit/comment/dto/CreateCommentDto.java
@@ -1,8 +1,11 @@
 package ru.practicum.shareit.comment.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class CreateCommentDto {
+
+    @NotBlank(message = "Комментарий не должен быть пустым")
     private String text;
 }
